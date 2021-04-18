7d8c24a0efd0f5b6bf15e3ce4482d6f592cba33f
tylerwooten <Tylerwooten@tamu.edu>
2021-04-18 14:13:15 -0500

    Creating standard or spot instance

---
+++ create-standard-instance.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+baseResourcesStackName=$1
+shift
+
+stackName=$1
+shift
+
+aws cloudformation deploy --stack-name $stackName --parameter-overrides ResourcesStackName=$baseResourcesStackName --template-file standard-instance.yaml
