ac2557ae8929948891dc34c92470798823889ace
MaikelGit <78486763+MaikelGit@users.noreply.github.com>
2022-07-22 15:44:41 -0300

    Update menu.sh
    - add interaction with config.ini not allowing to enter the menu without setting the team name in the file
    - removed options from the menu still under development
    - improved displaying current run.env and system.env settings

--- menu.sh
+++ menu.sh
@@ -1,7 +1,37 @@
 #!/bin/bash
 
-MY_BASE_RESOURCES_STACK=myteam-base-resources
-MY_BASE_RESOURCES_BUCKET=myteam-base-resources-bucket-1nj5uz43l2prj
+get_team_details()
+{
+    if [[ -f "./config.ini" ]];then
+        LINES=$(grep -v '^#' ./config.ini | grep -E '^MY_BASE_RESOURCES_STACK=|^MY_BASE_RESOURCES_BUCKET=')
+        if [[ "$(grep -E '^MY_BASE_RESOURCES_STACK=|^MY_BASE_RESOURCES_BUCKET=' ./config.ini | wc -l)" -ne "2" ]];then
+            echo -e "\e[1;32m File config.ini must contain variables MY_BASE_RESOURCES_STACK and MY_BASE_RESOURCES_BUCKET defined. \n Example
+                 MY_BASE_RESOURCES_STACK=myteam-base-resources
+                 MY_BASE_RESOURCES_BUCKET=myteam-base-resources-bucket-xxxxxxxx \e[0m  (this one is created by the tool once you did all the setup)"
+            exit 1 
+        fi
+        echo -e "File config.ini found with this configuration:"
+        for l in $LINES; do
+            env_var=$(echo $l | cut -f1 -d\=)
+            env_val=$(echo $l | cut -f2 -d\=)
+            eval export "$env_var=$env_val"
+            echo -e "\e[1;32m $env_var=$env_val \e[0m"
+        done
+        echo
+        read -p "Do you want to use this configuration to continue [Y/N]? " -n 1 -r
+        if ! [[ $REPLY =~ ^[Yy]$ ]];then
+            echo
+            echo -e "\e[1;32m ******** UPDATE File config.ini and try again *************** \e[0m"
+            exit 1
+        fi
+        echo
+        echo "**********************************************"
+    else
+        echo -e "\e[1;32m File config.ini does not exist. \e[0m"
+        exit 1
+    fi
+}
+
 
 submit_model_to_train()
 {
@@ -181,15 +211,17 @@ submit_model_to_train()
     echo "**** DEEPRACER EC2 INSTANCE LAUNCHER MENU ****"
     
     echo "##### SUMMARY REPORT ####################################"
-                echo -e "\tInstance Type selected = \e[1;32m $DEEPRACER_INSTANCE_TYPE \e[0m"
-                echo -e "\tInstance lifecycle selected = \e[1;32m $lifecycle \e[0m"
-                if [ $lifecycle = "standard" ]
-                then
-                  echo -e "\tMaximum Uptime selected (in minutes) = \e[1;32m $TimeToLiveInMinutes \e[0m"
-                fi
-    echo "##### ADDITIONALLY FOUND ################################"
-                echo -e "\tModel name to be trained = \e[1;32m $modelname \e[0m"
-                echo -e "\tNumber of workers = \e[1;32m $numberofworkers \e[0m"
+    echo -e "\tInstance Type selected = \e[1;32m $DEEPRACER_INSTANCE_TYPE \e[0m"
+    echo -e "\tInstance lifecycle selected = \e[1;32m $lifecycle \e[0m"
+    if [ $lifecycle = "standard" ];then
+        echo -e "\tMaximum Uptime selected (in minutes) = \e[1;32m $TimeToLiveInMinutes \e[0m"
+    fi
+    echo
+    echo "##### RUN.ENV     FREQUENTLY USED SETTINGS ##############"
+    grep -E '^DR_WORLD_NAME=|^DR_RACE_TYPE=|^DR_TRAIN_CHANGE_START_POSITION=|^DR_TRAIN_ALTERNATE_DRIVING_DIRECTION=|^DR_TRAIN_START_POSITION_OFFSET=|^DR_TRAIN_ROUND_ROBIN_ADVANCE_DIST=|^DR_LOCAL_S3_MODEL_PREFIX=|^DR_LOCAL_S3_PRETRAINED=|^DR_LOCAL_S3_PRETRAINED_PREFIX=|^DR_LOCAL_S3_PRETRAINED_CHECKPOINT=|^DR_UPLOAD_S3_PREFIX=|^DR_OA_NUMBER_OF_OBSTACLES=|^DR_OA_MIN_DISTANCE_BETWEEN_OBSTACLES=|^DR_OA_RANDOMIZE_OBSTACLE_LOCATIONS=|^DR_OA_IS_OBSTACLE_BOT_CAR=|^DR_OA_OBJECT_POSITIONS=|^DR_H2B_IS_LANE_CHANGE=|^DR_H2B_LOWER_LANE_CHANGE_TIME=|^DR_H2B_UPPER_LANE_CHANGE_TIME=|^DR_H2B_LANE_CHANGE_DISTANCE=|^DR_H2B_NUMBER_OF_BOT_CARS=|^DR_H2B_MIN_DISTANCE_BETWEEN_BOT_CARS=|^DR_H2B_RANDOMIZE_BOT_CAR_LOCATIONS=|^DR_H2B_BOT_CAR_SPEED=' ./custom-files/run.env
+    echo
+    echo "##### SYSTEM.ENV  FREQUENTLY USED SETTINGS ##############"
+    grep -E '^DR_UPLOAD_S3_BUCKET=|^DR_LOCAL_S3_BUCKET=|^DR_SAGEMAKER_IMAGE=|^DR_ROBOMAKER_IMAGE=|^DR_ANALYSIS_IMAGE=|^DR_COACH_IMAGE=|^DR_WORKERS=|^CUDA_VISIBLE_DEVICES=' ./custom-files/system.env
     echo "#########################################################"
     echo
     echo        
@@ -237,8 +269,10 @@ echo
 echo
 echo "**** DEEPRACER EC2 INSTANCE LAUNCHER MENU ****"
 
+get_team_details
+
 PS3='Choose your option: '
-MainOptions=("Create Base resources stack (only once)" "Create Image builder (only once)" "Submit model to train on EC2" "Add Access to EC2 from IP Address (limited number of entries)" "Download custom_files from S3 (overwrites CloudShell files with S3 files)" "Display configuration files" "Increment training at system.env (dr-increment-training)" "Exit")
+MainOptions=("Submit model to train on EC2" "Download custom_files from S3 (overwrites CloudShell files with S3 files)" "Exit")
 COLUMNS=0
 select main in "${MainOptions[@]}"; do
     case $main in
@@ -286,5 +320,3 @@ select main in "${MainOptions[@]}"; do
         *) echo -e "\t\e[1;33;1;41m invalid option $REPLY \e[0m";;
     esac
 done
-
-sleep 2 > /dev/null
