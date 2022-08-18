7adec8a220e7f86f73d01f8d464fad8ef151b6fb
Tyler Wooten <tylerwooten99@gmail.com>
2022-08-18 00:59:28 +0000

    get ec2 AMI from source account

--- create-standard-instance.sh
+++ create-standard-instance.sh
@@ -17,6 +17,7 @@ if [[ -n "$DEEPRACER_INSTANCE_TYPE" ]]; then
     instanceTypeConfig="InstanceType=$DEEPRACER_INSTANCE_TYPE"
 fi
 BUCKET=$(aws cloudformation describe-stacks --stack-name $baseResourcesStackName | jq '.Stacks | .[] | .Outputs | .[] | select(.OutputKey=="Bucket") | .OutputValue' | tr -d '"')
+amiId=$(aws ec2 describe-images --owners 747447086422 --query 'sort_by(Images, &CreationDate)[0].ImageId' | tr -d '"')
 
 set +xa
 
@@ -39,7 +40,7 @@ fi
 set -x
 
 aws s3 cp custom-files s3://${BUCKET}/custom_files --recursive
-aws cloudformation deploy --stack-name $stackName --parameter-overrides ${instanceTypeConfig} ResourcesStackName=$baseResourcesStackName TimeToLiveInMinutes=$timeToLiveInMinutes --template-file standard-instance.yaml
+aws cloudformation deploy --stack-name $stackName --parameter-overrides ${instanceTypeConfig} ResourcesStackName=$baseResourcesStackName TimeToLiveInMinutes=$timeToLiveInMinutes AmiId=$amiId --template-file standard-instance.yaml
 EC2_IP=`aws cloudformation list-exports --query "Exports[?Name=='${stackName}-PublicIp'].Value" --no-paginate --output text`
 echo "Logs will upload every 2 minutes to https://s3.console.aws.amazon.com/s3/buckets/${BUCKET}/${stackName}/logs/"
 echo "Training should start shortly on ${EC2_IP}:8080"
