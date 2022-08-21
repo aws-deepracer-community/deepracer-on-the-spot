4db040a700edc1ea7d60c61b5137ad337ffa0e3b
Tyler Wooten <tylerwooten99@gmail.com>
2022-08-21 20:49:21 +0000

    working spot and standard from source image

--- create-standard-instance.sh
+++ create-standard-instance.sh
@@ -42,7 +42,7 @@ fi
 set -x
 
 aws s3 cp custom-files s3://${BUCKET}/custom_files --recursive
-aws cloudformation deploy --stack-name $stackName --parameter-overrides ${instanceTypeConfig} ResourcesStackName=$baseResourcesStackName TimeToLiveInMinutes=$timeToLiveInMinutes AmiId=$amiId --template-file standard-instance.yaml
+aws cloudformation deploy --stack-name $stackName --parameter-overrides ${instanceTypeConfig} ResourcesStackName=$baseResourcesStackName TimeToLiveInMinutes=$timeToLiveInMinutes AmiId=$amiId BUCKET=$BUCKET --template-file standard-instance.yaml
 EC2_IP=`aws cloudformation list-exports --query "Exports[?Name=='${stackName}-PublicIp'].Value" --no-paginate --output text`
 echo "Logs will upload every 2 minutes to https://s3.console.aws.amazon.com/s3/buckets/${BUCKET}/${stackName}/logs/"
 echo "Training should start shortly on ${EC2_IP}:8080"
