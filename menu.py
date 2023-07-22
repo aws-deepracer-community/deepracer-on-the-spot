937263053816f0009c2a233a683dfcf651937208
Tyler Wooten <tylerwooten99@gmail.com>
2023-07-22 23:35:42 +0000

    refactoring and error handling for menu

--- menu.py
+++ menu.py
@@ -1,7 +1,6 @@
 
 import json, os
 
-
 # ENV Config files functions
 
 def write_env_variable(file, variable, value):
@@ -149,18 +148,21 @@ def menu_picker(label,options,custom):
         else:
             print("Invalid input. Please enter a valid number.")
 
-
-def add_ip(options):
-    stack=read_env_variable(options[12]['file'], options[12]['key'])
+def add_ip():
+    stack=read_env_variable(OPTIONS['13']['file'], OPTIONS['13']['key'])
+    if stack is None:
+        select_option(OPTIONS['13'])
     ipname=input("Enter a name for the request: ")
     ip=input("Enter your IP: ")
     os.chdir('scripts')
     os.system("./add-access.sh {} {} {}".format(stack,ipname,ip))
     os.chdir('..')
 
-def run_training(options,pretrained):
+def run_training(pretrained):
     envfile="custom-files/run.env"
-    stack=read_env_variable(options[12]['file'], options[12]['key'])
+    stack=read_env_variable(OPTIONS['13']['file'], OPTIONS['13']['key'])
+    if stack is None:
+        select_option(OPTIONS['13'])
     modelname=read_env_variable(envfile, "DR_LOCAL_S3_MODEL_PREFIX")
     pre_modelname=read_env_variable(envfile, "DR_LOCAL_S3_PRETRAINED_PREFIX") 
     if pretrained==False:
@@ -180,16 +182,12 @@ def run_training(options,pretrained):
       write_env_variable(envfile, "DR_LOCAL_S3_MODEL_PREFIX",modelname)
       write_env_variable(envfile, "DR_LOCAL_S3_PRETRAINED_PREFIX",pre_modelname)
 
-      
-
-    if stack is None:
-      select_option(options[int(13)-1])
     print()
     standarspot=menu_picker("Select EC2 type: ",[  "spot", "standard" ],False)
     print()
-    machinetype=menu_picker("Pick HW configuration: ",[  "g4dn.2xlarge", "g4dn.4xlarge", "g4dn.8xlarge", "g4dn.12xlarge", "g5.2xlarge", "g5.4xlarge", "g5.8xlarge", "g5.12xlarge" ],True)
+    machinetype=menu_picker("Pick HW configuration: ",[  "g4dn.2xlarge (RECOMMENDED. any larger will be more expensive)", "g4dn.4xlarge", "g4dn.8xlarge", "g4dn.12xlarge", "g5.2xlarge", "g5.4xlarge", "g5.8xlarge", "g5.12xlarge" ],True)
     print()
-    stack=read_env_variable(options[12]['file'], options[12]['key'])
+    stack=read_env_variable(OPTIONS['13']['file'], OPTIONS['13']['key'])
     print()
     wait=input("Insert Time to live (Minutes):")
     print("./create-{}-instance.sh {} {} {}".format(standarspot,stack,modelname,wait))
@@ -213,72 +211,65 @@ def set_new_reward():
 # Function to show menu
 def show_menu():
     while True:
-      print(" ")
-      print("\n--- AWS Console (CLI Version) ---")
-      print(" ")
-      print("---Configuration---")
+        print(" ")
+        print("\n--- AWS Console (CLI Version) ---")
+        print(" ")
+        print("---Configuration---")
 
-      options = [
-          {"option": "1", "label": "Modify Model Name","file": "custom-files/run.env", "key": "DR_LOCAL_S3_MODEL_PREFIX", "dtype" : "string"},
-          {"option": "2", "label": "Modify Car Name","file": "custom-files/run.env", "key": "DR_CAR_NAME", "dtype" : "string"},
-          {"option": "3", "label": "Modify Circuit","file": "custom-files/run.env", "key": "DR_WORLD_NAME", "dtype" : "string"},
-          {"option": "4", "label": "Modify Race Type (TT,OA,H2H)","file": "custom-files/run.env", "key": "DR_RACE_TYPE", "dtype" : "string"},
-          {"option": "5", "label": "Modify Batch Size", "file": "custom-files/hyperparameters.json", "key": "batch_size", "dtype" : "int"},
-          {"option": "6", "label": "Modify Beta_entropy", "file": "custom-files/hyperparameters.json", "key": "beta_entropy", "dtype" : "float"},
-          {"option": "7", "label": "Modify Discount Factor", "file": "custom-files/hyperparameters.json", "key": "discount_factor", "dtype" : "float"},
-          {"option": "8", "label": "Modify Loss Type", "file": "custom-files/hyperparameters.json", "key": "loss_type", "dtype" : "string"},
-          {"option": "9", "label": "Modify Learning Rate", "file": "custom-files/hyperparameters.json", "key": "lr", "dtype" : "float"},
-          {"option": "10", "label": "Modify Num of episodes between training", "file": "custom-files/hyperparameters.json", "key": "num_episodes_between_training", "dtype" : "int"},
-          {"option": "11", "label": "Modify Num Epochs", "file": "custom-files/hyperparameters.json", "key": "num_epochs", "dtype" : "int"},
-          {"option": "12", "label": "Modify Action Space", "file": "custom-files/model_metadata.json", "key": "action_space", "dtype" : "array"},
-          {"option": "13", "label": "Modify Stack", "file": "custom-files/run.env", "key": "STACK", "dtype" : "string"}
-      ]
-      
+        for option_num, option in OPTIONS.items():
+            file, key, label = option.get('file',''), option.get('key',''), option.get('label','')
+            if '.env' in file:
+                current_value = read_env_variable(file, key)
+            elif '.json' in file:
+                current_value = read_json_value(file, key)
+            else:
+                current_value = ""
+            comparison = "("+str(key)+'=\033[93m'+ str(current_value)+"\033[0m)" if key or current_value else ""
+            print("{} {} {}".format(str(option_num).ljust(2), str(label).ljust(40), comparison))
 
-      for option in options:
-          if '.env' in option['file']:
-              current_value = read_env_variable(option['file'], option['key'])
-          elif '.json' in option['file']:
-              current_value = read_json_value(option['file'], option['key'])
-          else:
-              current_value = ""
-          print("{} {} {}".format(str(option['option']).ljust(2), str(option['label']).ljust(40), "("+str(option['key'])+'=\033[93m'+ str(current_value))+"\033[0m)")
-          
-      print("14 Set new Reward Function")
-      print("15 Add IP Access")
-      print(" ")
-      print("---Train---")      
-      print("16 Run New Training")
-      print("17 Continue a Training")
-      print("0 QUIT")
-      print(" ")
-      choice = input("Pick a menu item: ")
+        try:
+            choice = str(input("Pick a menu item (0-{}): ".format(len(OPTIONS)-1)))
+        except KeyboardInterrupt:
+            print("\n")
+            break
+        if not isinstance(choice, str) or choice not in list(OPTIONS.keys()):
+            print("invalid input:", choice)
+     
+        if choice == "0":
+            break
 
-      if choice == "0":
-        break
-      elif choice == "14":
-        set_new_reward()
-        break
-      elif choice == "15":
-        add_ip(options)
-        break
-      elif choice == "16":
-        run_training(options,False)
-        break
-      elif choice == "17":
-        run_training(options,True)
-        break
-              
-      else:
-        select_option(options[int(choice)-1])
- 
+        if choice:
+            selection = OPTIONS.get(choice)
+            if selection.get('func'):
+                func = selection.get('func')
+                args = selection.get('args','')
+                func(*args)
+                break
+            else:
+                select_option(selection) # LEFT OFF HERE. need to follow down select_option and update to use OPTIONS. also func() should not need options passed in as an arg
+
+OPTIONS = {
+          "1":{ "label": "Modify Model Name","file": "custom-files/run.env", "key": "DR_LOCAL_S3_MODEL_PREFIX", "dtype" : "string"},
+          "2":{ "label": "Modify Car Name","file": "custom-files/run.env", "key": "DR_CAR_NAME", "dtype" : "string"},
+          "3":{ "label": "Modify Circuit","file": "custom-files/run.env", "key": "DR_WORLD_NAME", "dtype" : "string"},
+          "4":{ "label": "Modify Race Type (TT,OA,H2H)","file": "custom-files/run.env", "key": "DR_RACE_TYPE", "dtype" : "string"},
+          "5":{ "label": "Modify Batch Size", "file": "custom-files/hyperparameters.json", "key": "batch_size", "dtype" : "int"},
+          "6":{ "label": "Modify Beta_entropy", "file": "custom-files/hyperparameters.json", "key": "beta_entropy", "dtype" : "float"},
+          "7":{ "label": "Modify Discount Factor", "file": "custom-files/hyperparameters.json", "key": "discount_factor", "dtype" : "float"},
+          "8":{ "label": "Modify Loss Type", "file": "custom-files/hyperparameters.json", "key": "loss_type", "dtype" : "string"},
+          "9":{ "label": "Modify Learning Rate", "file": "custom-files/hyperparameters.json", "key": "lr", "dtype" : "float"},
+          "10":{ "label": "Modify Num of episodes between training", "file": "custom-files/hyperparameters.json", "key": "num_episodes_between_training", "dtype" : "int"},
+          "11":{ "label": "Modify Num Epochs", "file": "custom-files/hyperparameters.json", "key": "num_epochs", "dtype" : "int"},
+          "12":{ "label": "Modify Action Space", "file": "custom-files/model_metadata.json", "key": "action_space", "dtype" : "array"},
+          "13":{ "label": "Modify Base Stack Name", "file": "custom-files/run.env", "key": "BASE_STACK_NAME", "dtype" : "string"},
+          "14":{ "label": "Set New Reward Function", "func": set_new_reward},
+          "15":{ "label": "Add IP Access", "func": add_ip},
+          "16":{ "label": "Run New Training", "func": run_training, "args": (False,)},
+          "17":{ "label": "Continue A Training", "func": run_training, "args": (True,)},
+          "0" :{ "label": "Quit"}
+        }
 
 # Execute Menu
-def menu():
+if __name__=='__main__':
+    show_menu()
 
-        show_menu()
-
-
-
-
-menu()
