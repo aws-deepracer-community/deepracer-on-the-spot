765186b53a6d7ac9ba06b9704cc1aa22dc880191
Mark <mark.ross@eviden.com>
2024-05-05 18:45:53 +0100

    refactored base

--- create-base-resources.sh
+++ create-base-resources.sh
@@ -25,17 +25,8 @@ if [[ "$stackName" == "" || "$ip" == "" || "$ruleN" == "" ]]; then
 	exit 1
 fi
 
-subnetsConfig=''
-
 vpc=$(aws ec2 describe-vpcs --filters Name=isDefault,Values=true --query 'Vpcs[*].VpcId' --output text)
-subnetCount=$(aws ec2 describe-subnets --filters Name=vpc-id,Values=$vpc --query 'Subnets[*].SubnetId' | grep subnet | wc -l)
-index=0
-while [[ $index -lt $subnetCount ]];do
-	subnetId=$(aws ec2 describe-subnets --filters Name=vpc-id,Values=$vpc --query "Subnets[$index].SubnetId" --output text)
-	subnetsConfig="${subnetsConfig} Subnet${index}=$subnetId"
-	index=$(expr $index + 1)
-done
 
 nacl=$(aws ec2 describe-network-acls --filters Name=vpc-id,Values=$vpc --query 'NetworkAcls[*].NetworkAclId' --output text)
 
-aws cloudformation deploy --template ./base-resources.yaml --stack-name $stackName --parameter-overrides ${subnetsConfig} VPC=$vpc MyIPAddress=$ip NetworkAclId=$nacl RuleNumber=$ruleN --capabilities CAPABILITY_IAM
+aws cloudformation deploy --template ./base-resources.yaml --stack-name $stackName --parameter-overrides MyIPAddress=$ip NetworkAclId=$nacl RuleNumber=$ruleN --capabilities CAPABILITY_IAM
