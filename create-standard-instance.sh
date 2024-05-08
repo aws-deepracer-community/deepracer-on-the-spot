7dd3625d53b77212607c953f6116478c7c086503
Mark <mark.ross@eviden.com>
2024-05-08 20:46:21 +0100

    port changes to standard instance

--- create-standard-instance.sh
+++ create-standard-instance.sh
@@ -44,7 +44,7 @@ chmod +x ./validation.sh
 
 if [[ $? -ne 0 ]]; then
     while true; do
-        echo -e "\e[1;33m  ##########  Error found in reward_function.py, want to continue anyway? \e[0m"
+        echo -e "\e[1;33m  ##########  Error found in your custom files, want to continue anyway? \e[0m"
         read -p "[y / n]: " yn
         case $yn in
             [Yy]* ) break;;
@@ -56,9 +56,9 @@ fi
 
 set -x
 
-CUSTOM_FILE_LOCATION=$(cat custom-files/run.env | grep DR_LOCAL_S3_CUSTOM_FILES_PREFIX= | awk -F'=' '{print $2}')
-aws s3 cp custom-files s3://${BUCKET}/${CUSTOM_FILE_LOCATION} --recursive
-aws cloudformation deploy --stack-name $stackName --parameter-overrides ${instanceTypeConfig} ResourcesStackName=$baseResourcesStackName DeepRacerImportName=$stackName TimeToLiveInMinutes=$timeToLiveInMinutes AmiId=$amiId BUCKET=$BUCKET CUSTOMFILELOCATION=$CUSTOM_FILE_LOCATION --template-file standard-instance.yaml --s3-bucket $BUCKET --s3-prefix cf_templates
+source custom-files/run.env
+aws s3 cp custom-files s3://${BUCKET}/${DR_LOCAL_S3_CUSTOM_FILES_PREFIX} --recursive
+aws cloudformation deploy --stack-name $stackName --parameter-overrides ${instanceTypeConfig} ResourcesStackName=$baseResourcesStackName DeepRacerImportName=$stackName TimeToLiveInMinutes=$timeToLiveInMinutes AmiId=$amiId BUCKET=$BUCKET CUSTOMFILELOCATION=$DR_LOCAL_S3_CUSTOM_FILES_PREFIX --template-file standard-instance.yaml --s3-bucket $BUCKET --s3-prefix cf_templates
 EC2_IP=`aws cloudformation list-exports --query "Exports[?Name=='${stackName}-PublicIp'].Value" --no-paginate --output text`
 echo "Logs will upload every 2 minutes to https://s3.console.aws.amazon.com/s3/buckets/${BUCKET}/${stackName}/logs/"
 echo "Training should start shortly on ${EC2_IP}:8080"
