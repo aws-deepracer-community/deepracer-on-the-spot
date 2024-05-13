0140bc9323257c6752fd285b536d401b3c9d1e4b
Mark <mark.ross@eviden.com>
2024-05-13 08:20:18 +0100

    update openl validation

--- validation.sh
+++ validation.sh
@@ -18,7 +18,7 @@ fi
 source custom-files/system.env
 if [ $DR_HOST_X == True ]
 then
-  if [ $DR_GUI_ENABLE != True ] || [[ $DR_SAGEMAKER_IMAGE != *"gpu"* ]] || [[ $DR_SAGEMAKER_CUDA_DEVICES != 0 ]] || [[ $DR_ROBOMAKER_CUDA_DEVICES != 0 ]]
+  if [ $DR_GUI_ENABLE != True ] || [[ $DR_SAGEMAKER_IMAGE != *"gpu"* ]] || [[ $DR_SAGEMAKER_CUDA_DEVICES != 0 ]] || [[ $DR_ROBOMAKER_CUDA_DEVICES != 0 ]] || [ $DR_DOCKER_STYLE != compose ]
   then
     echo "Incorrect settings for OpenGL, please update system.env"
     echo "either set DR_HOST_X=False to disabled OpenGL GPU training"
