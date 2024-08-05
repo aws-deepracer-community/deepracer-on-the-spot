7818213bad752507553d4922cca4eaa123b0df1c
unknown <mpalazon3@gmail.com>
2024-08-05 10:47:11 -0300

    corrected typo and types

--- menu.py
+++ menu.py
@@ -266,11 +266,9 @@ OPTIONS = {
           "11":{ "label": "Modify Num Epochs", "file": "custom-files/hyperparameters.json", "key": "num_epochs", "dtype" : "int"},
           "12":{ "label": "Modify Action Space", "file": "custom-files/model_metadata.json", "key": "action_space", "dtype" : "array"},
           "13":{ "label": "Modify Base Stack Name", "file": "custom-files/run.env", "key": "BASE_STACK_NAME", "dtype" : "string"},
-          
-          "14":{ "label": "Modify Round Robin Advance Dist (Workers/Epsidoes-Between-Training)", "file": "custom-files/run.env", "key": "DR_TRAIN_ROUND_ROBIN_ADVANCE_DIST", "dtype" : "string"},
-          "15":{ "label": "Modify Minimum Training Amount", "file": "custom-files/run.env", "key": "DR_TRAIN_MIN_EVAL_TRIALS", "dtype" : "string"},
+          "14":{ "label": "Modify Round Robin Advance Dist (Workers/Episodes-Between-Training)", "file": "custom-files/run.env", "key": "DR_TRAIN_ROUND_ROBIN_ADVANCE_DIST", "dtype" : "float"},
+          "15":{ "label": "Modify Minimum Training Amount", "file": "custom-files/run.env", "key": "DR_TRAIN_MIN_EVAL_TRIALS", "dtype" : "int"},
           "16":{ "label": "Modify Checkpoint Priorizing Criteria", "file": "custom-files/run.env", "key": "DR_LOCAL_S3_PRETRAINED_CHECKPOINT", "dtype" : "string"},
-
           "17":{ "label": "Set New Reward Function", "func": set_new_reward},
           "18":{ "label": "Add IP Access", "func": add_ip},
           "19":{ "label": "Run New Training", "func": run_training, "args": (False,)},
