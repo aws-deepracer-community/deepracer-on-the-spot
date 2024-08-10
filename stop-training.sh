87635e62569286889ae6d20b1ac926330c6f089d
Pablo J. Rogina <43066+pablojr@users.noreply.github.com>
2024-08-10 21:34:13 +0000

    Changed stop script to check for aws command return code; validated at least one argument is provided

--- stop-training.sh
+++ stop-training.sh
@@ -2,6 +2,11 @@
 
 set -xa
 
+if [ $# -eq 0 ]; then
+   echo "Required model name to stop training not provided"
+   exit 1
+fi
+
 stackName=$1
 shift
 
@@ -9,4 +14,10 @@ timeToLiveInMinutes=2
 shift
 
 aws cloudformation update-stack --stack-name $stackName --use-previous-template --parameters  ParameterKey=TimeToLiveInMinutes,ParameterValue=$timeToLiveInMinutes ParameterKey=BUCKET,UsePreviousValue=true ParameterKey=ResourcesStackName,UsePreviousValue=true ParameterKey=DeepRacerImportName,UsePreviousValue=true ParameterKey=CUSTOMFILELOCATION,UsePreviousValue=true ParameterKey=AmiId,UsePreviousValue=true --capabilities CAPABILITY_IAM
-echo "Training will stop in " $timeToLiveInMinutes " minutes"
+
+if [ $? -eq 0 ]; then
+    echo "Training will stop in " $timeToLiveInMinutes " minutes"
+else
+    echo "Error trying to stop training"
+fi
+
