0b7247ab8b1f0adf004462ffc1b1acd26427e9bc
MarkRoss-Eviden <53598199+MarkRoss-Eviden@users.noreply.github.com>
2024-08-12 11:42:44 +0100

    Feature/optional import (#156)
    * optional import
    
    * updated documentation

--- create-standard-instance.sh
+++ create-standard-instance.sh
@@ -63,4 +63,4 @@ EC2_IP=`aws cloudformation list-exports --query "Exports[?Name=='${stackName}-Pu
 echo "Logs will upload every 2 minutes to https://s3.console.aws.amazon.com/s3/buckets/${BUCKET}/${stackName}/logs/"
 echo "Training should start shortly on ${EC2_IP}:8080"
 echo "Once started, you should also be able to monitor training progress through ${EC2_IP}:8100/menu.html"
-echo "Once training is finished, you should see your imported model $stackName in the DeepRacer console"
+echo "Once training is finished, you should see your imported model $stackName in the DeepRacer console, unless you disable that functionality"
