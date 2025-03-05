f7610544910af270dce49e560de9f1658e79a159
Mark Ross <mark.ross@atos.net>
2025-03-05 16:01:36 +0000

    update flask app host

--- website/backend/app.py
+++ website/backend/app.py
@@ -18,4 +18,4 @@ def run_command():
         return jsonify({'error': str(e), 'output': e.output}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
